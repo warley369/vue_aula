287df54f821466e91bb40badfb44ebeb634863b8
warley junior <111403547+warley369@users.noreply.github.com>
2025-04-04 14:20:47 +0000

    ok

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -18,4 +18,21 @@ export default {
   font-family: 'RobotoMono', monospace;
   src: url("./fonts/RobotoMono-Thin.ttf");
 }
+* {
+  font-family: 'RobotoMono', monospace;
+}
+body {
+  margin: 0;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #fff;
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+
+  }
 </style>
